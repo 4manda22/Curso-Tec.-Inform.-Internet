94aa350de397aa8ad684159ed29c0b325325673b
Amanda Mikaele Lira <amandamikaelelira@gmail.com>
2024-01-15 10:05:49 -0300

    atividade001

---
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 02/Exemplo 01/Atividade-01.js
@@ -0,0 +1,24 @@
+//1-Q 
+/*valor = 8
+alert(3+5, valor)*/
+
+//2-Q
+/*let nome = "Alice"
+let idade = 30
+alert( nome + " tem " + idade +  " anos. ")*/
+
+//3-Q
+
+/*let preço = 24.99
+let quantidade = 5
+alert(preço * quantidade)*/
+
+//4-Q
+
+
+//5-Q
+/*let nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+let altura = prompt("Qual sua altura?")
+
+alert(" Meu nome é " + nome + " tenho " + idade + " anos e" + altura + " metros de altura " )*/
