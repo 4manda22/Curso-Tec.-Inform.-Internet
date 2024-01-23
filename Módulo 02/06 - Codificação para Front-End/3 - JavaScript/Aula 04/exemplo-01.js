b540af05070bd99f648a7fffc1e405dd1e5c4a13
Amanda Mikaele Lira <amandamikaelelira@gmail.com>
2024-01-23 10:58:11 -0300

    trrtu

--- Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 04/exemplo-01.js
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 04/exemplo-01.js
@@ -1,9 +1,9 @@
 function clicou(){
     document.getElementById("t3").textContent = parseInt(Math.random() * 11 )
 
-    let r = 40
-    let g = 35
-    let b = 200
+    let r = parseInt(Math.random() * 256)
+    let g = parseInt(Math.random() * 256)
+    let b = parseInt(Math.random() * 256)
     document.getElementById("t3").style.color = `rgb(${r}, ${g}, ${b})`
 
 }
