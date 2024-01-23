da6d854627518218316c856521f69e6d87f6a0cb
Amanda Mikaele Lira <amandamikaelelira@gmail.com>
2024-01-23 09:06:58 -0300

    jklkkl

---
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 02/Aula 03/exemplo-03.js
@@ -0,0 +1,26 @@
+/* Q-01*/
+
+/*let lista = ["gato", "ovelha", "cachorro"]
+for(let x in lista){
+    if(lista [x]== "gato")
+        alert("verdadeiro")
+}
+*/
+
+/*Q-02*/
+
+/*let lista = [2,7,4,9,1]
+for (let x in lista){
+    if (lista[x] % 2 == 0){
+        alert (lista[x] + "é par")
+    }else{
+        alert(lista[x] + "é ímpar")
+    }
+}
+
+/*Q-03*/
+
+/*for (let x = 15; x >= 3; x--){
+    alert(x)
+}
+*/
