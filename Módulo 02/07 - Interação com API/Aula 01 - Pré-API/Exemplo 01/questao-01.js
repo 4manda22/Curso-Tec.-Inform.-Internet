51595aa8c3fb4d3f3f16c2469b358f61e6dfdb02
Amanda Mikaele Lira <amandamikaelelira@gmail.com>
2024-01-26 10:11:32 -0300

    jgyyy

--- Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 01/questao-01.js
+++ Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 01/questao-01.js
@@ -1,9 +1,10 @@
-
+/*
 setTimeout(teste, 3000)
 
 function teste(){
     alert("oi")
 }
+*/
 
 
 /*
@@ -13,3 +14,21 @@ function teste(){
     document.getElementById("demo").textContent = document.getElementById("demo").textContent + "Testando"
 }
 */
+
+/*
+
+setTimeout(avisar, 2000)
+
+function avisar(){
+    alert("estuuuda")
+}
+avisar()
+*/
+
+
+setInterval(acionar, 2000)
+
+function acionar(){
+    let elemento = document.getElementById("demo")
+    elemento.textContent = elemento.textContent + "Senai "
+}
